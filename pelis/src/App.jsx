900d5b471fceab655dda7bcb3ea1fefdacc54f12
AngelUrrego <angelurrego2010@gmail.com>
2024-05-27 19:13:34 -0500

    Tarea-4

--- pelis/src/App.jsx
+++ pelis/src/App.jsx
@@ -1,29 +1,52 @@
-import React from 'react';
-import img from './assets/spiderman.jpg'
-import Title from './componentes/title';
-import Img from './componentes/img';
-import image from './assets/rick.jpg'
-import image2 from './assets/morty.jpg'
-import Details from './componentes/details';
+/* eslint-disable react/prop-types */
+import { useState, useEffect } from 'react'
+import Card from './componentes/card'
 import './App.css'
+import rick from './assets/rick.png'
+import morty from './assets/morty.png'
+
 
 function App() {
+  const [pagina, setPagina] = useState(0)
+  const [personajes, setPersonajes] = useState([])
+  useEffect(() => {
+    fetch(`https://rickandmortyapi.com/api/character/?page=${pagina}`)
+      .then((response) => response.json())
+      .then((data) => setPersonajes(data.results))
+  }, [pagina])
+  const incrementarContador = () => {
+    setPagina(pagina + 1)
+  }
+  const decrementarContador = () => {
+    setPagina(pagina - 1)
+  }
   return (
     <>
-    <div className='container'>
-      <div className='card'>
-        <Img url={image}/>
-        <Title title="Rick Sánchez" />
-        <Details genre="Humano" status="vivo"/>
+      <div className='container'>
+        {personajes.length !== 0 && personajes.map((personaje) => (
+          <Card 
+          key={personaje.id} 
+          url={personaje.image} 
+          title={personaje.name} 
+          genre={personaje.gender} 
+          status={personaje.status}/>
+        )) }
       </div>
-      <div className='card'>
-        <Img url={image2}/> 
-        <Title title="Morty Smith" />
-        <Details genre="Humano" status="vivo"/>
+      <div className='containerButton'>
+        <button onClick={decrementarContador}>
+        <img className="rick" src={morty} alt="morty" />
+        Anteior Pagina
+        </button>
+        
+        <button onClick={incrementarContador}>
+        <img className="rick" src={rick} alt="rick sanchez"/>
+        Siguiente Pagina
+        </button>
+        
       </div>
-    </div>
     </>
-  );
+  )
 }
 
-export default App;
+
+export default App
