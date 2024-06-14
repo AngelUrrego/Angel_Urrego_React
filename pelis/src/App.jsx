e13a3291e5bbc00c4c35571f0493e6d99956514c
AngelUrrego <angelurrego2010@gmail.com>
2024-06-13 19:22:36 -0500

    Tarea-7

--- pelis/src/App.jsx
+++ pelis/src/App.jsx
@@ -1,120 +1,33 @@
-import { useState, useEffect } from 'react';
-import Card from './componentes/card';
-import './App.css';
-import rick from './assets/rick.png';
-import morty from './assets/morty.png';
-import serie from './assets/serie.png';
+import {Link, Route, BrowserRouter as Router, Routes} from 'react-router-dom'
+import './styles/App.css'
+import './styles/navbar.css'
+import Home from './pages/Home'
+import Characters from './pages/Characters'
+import Episodes from './pages/Episodes'
+import serie from './assets/serie.png'
+import NotFound from './pages/NotFound'
+import Location from './pages/Location'
 
 function App() {
-  const [pagina, setPagina] = useState(1);
-  const [personajes, setPersonajes] = useState([]);
-  const [nameFilter, setNameFilter] = useState('');
-  const [genreFilter, setGenreFilter] = useState('');
-  const [statusFilter, setStatusFilter] = useState('');
-
-  useEffect(() => {
-    const filterParams = new URLSearchParams({
-      page: pagina,
-      name: nameFilter,
-      gender: genreFilter,
-      status: statusFilter,
-
-    });
-
-    fetch(`https://rickandmortyapi.com/api/character/?${filterParams}`)
-      .then((response) => response.json())
-      .then((data) => setPersonajes(data.results))
-  }, [pagina, nameFilter, genreFilter,statusFilter]);
-
-  const queryIncrementor = () => {
-    const url = new URL(window.location)
-    url.searchParams.set('page',parseInt(pagina)+ 1)
-    window.history.pushState({},'',url)
-    setPagina(parseInt(pagina)+ 1) 
-   }
-
-   const queryDecrementor = () =>{
-    const url = new URL(window.location)
-    url.searchParams.set('page', parseInt(pagina) - 1)
-    window.history.pushState({},'',url)
-    setPagina(parseInt(pagina) - 1)
-   }
-
-  const onSearch = (e) => {
-    e.preventDefault();
-    setPagina(1); // Reset to first page on new search
-    const url = new URL(window.location);
-    url.searchParams.set('name', nameFilter);
-    url.searchParams.set('gender', genreFilter);
-    url.searchParams.set('gender', statusFilter);
-    window.history.pushState({}, '', url);
-  };
-
   return (
-    <>
-      <div className='containerImg'>
-        <img src={serie} alt="" />
-      </div>
-      
-      <div className='forms'>
-        <form onSubmit={onSearch}>
-          <input
-          placeholder='Ingrese un nombre:'
-            type="text"
-            value={nameFilter}
-            onChange={(e) => setNameFilter(e.target.value)}
-          />
-          <select
-            value={genreFilter}
-            onChange={(e) => setGenreFilter(e.target.value)}
-          >
-           <option value="" disabled selected hidden>Seleccionar género</option>
-            <option value="">Todos</option>
-            <option value="Male">Hombre</option>
-            <option value="Female">Mujer</option>
-            <option value="Genderless">Sin género</option>
-            <option value="unknown">Desconocido</option>
-          </select>
-
-          <select
-            value={statusFilter}
-            onChange={(e) => setStatusFilter(e.target.value)}
-          >
-            <option value="" disabled selected hidden>Estado</option>
-            <option value="">Todos</option>
-            <option value="Alive">Vivo</option>
-            <option value="Dead">Muerto</option>
-            <option value="unknown">Desconocido</option>
-          </select>
-          <button >Buscar</button>
-        </form>
-
-      </div>
-      <div className='container'>
-        {personajes.length !== 0 && personajes.map((personaje) => (
-          <Card 
-            key={personaje.id} 
-            url={personaje.image} 
-            title={personaje.name} 
-            genre={personaje.gender} 
-            status={personaje.status}
-          />
-        ))}
-      </div>
-
-      <div className='containerButton'>
-        <button onClick={queryDecrementor} disabled={pagina === 1}>
-          <img className="rick" src={morty} alt="morty" />
-          Página Anterior
-        </button>
-
-        <button onClick={queryIncrementor}>
-          <img className="rick" src={rick} alt="rick sanchez"/>
-          Siguiente Página
-        </button>
-      </div>
-    </>
-  );
+    <Router>
+        <div className='nav'>
+              <img  src={serie} alt="" />  
+              <Link className="link" to="/">Inicio</Link>
+              <Link className="link" to='/characters'>Personajes</Link>
+              <Link className="linkE" to='/episodes'>Episodios</Link>
+              <Link className="linkE" to='/location'>Locaciones</Link>
+        </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/characters" element={<Characters />} />
+        <Route path="/episodes" element={<Episodes />} />
+        <Route path="/location" element={<Location />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+    
+  )
 }
 
-export default App;
+export default App
